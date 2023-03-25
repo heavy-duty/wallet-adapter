3f3cc24c3dda6afb51e93662d1c4d47de5c29e58
Daniel Marin <danielarturomt@gmail.com>
2023-03-25 12:16:37 +0000

    chore: bump dependencies (#1)
    * chore: bump core dependencies
    
    * chore: change each package version and update core readme

--- packages/ui/material/jest.config.ts
+++ packages/ui/material/jest.config.ts
@@ -3,15 +3,16 @@ export default {
   displayName: 'ui-material',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
+  globals: {},
   coverageDirectory: '../../../coverage/packages/ui/material',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
