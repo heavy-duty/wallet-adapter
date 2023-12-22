3c94a6a8979ca987c894afaef4c758e30551543a
Dan <danielarturomt@gmail.com>
2023-12-22 17:47:36 +0000

    fix: remove unnused imports

--- packages/cdk-example/src/app/app.config.ts
+++ packages/cdk-example/src/app/app.config.ts
@@ -1,12 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideWalletAdapter } from '@heavy-duty/wallet-adapter';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
-import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare';
+
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideWalletAdapter({
-      autoConnect: false,
-      adapters: [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
-    }),
-  ],
+  providers: [provideWalletAdapter()],
 };
