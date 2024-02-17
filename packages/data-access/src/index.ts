5af6e4fc1e0fa1194dc00077377a7de72895fbab
Dan <danielarturomt@gmail.com>
2024-02-17 23:32:31 +0000

    feat: add support for inject signal based methods

--- packages/data-access/src/index.ts
+++ packages/data-access/src/index.ts
@@ -1,4 +1,7 @@
 export * from './lib/connection.store';
+export * from './lib/inject-connection';
+export * from './lib/inject-public-key';
+export * from './lib/inject-transaction-sender';
 export * from './lib/standard-wallet-adapters.store';
 export * from './lib/wallet-adapter.module';
 export * from './lib/wallet.store';
