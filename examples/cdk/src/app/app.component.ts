bcf8daf588a9895e81d747d66797e028904a437a
Dan <danielarturomt@gmail.com>
2024-02-25 15:01:02 +0000

    refactor(cdk-example): :art: move multi-button to its own file

--- examples/cdk/src/app/app.component.ts
+++ examples/cdk/src/app/app.component.ts
@@ -1,21 +1,12 @@
-import { Dialog } from '@angular/cdk/dialog';
 import { NgClass } from '@angular/common';
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import {
-  WalletStore,
   injectConnected,
   injectPublicKey,
   injectWallet,
 } from '@heavy-duty/wallet-adapter';
-import {
-  HdConnectWalletDirective,
-  HdDisconnectWalletDirective,
-  HdObscureAddressPipe,
-  HdWalletIconComponent,
-} from '@heavy-duty/wallet-adapter-cdk';
-import { WalletName } from '@solana/wallet-adapter-base';
-import { EMPTY, concatMap } from 'rxjs';
-import { WalletsModalComponent } from './wallets-modal.component';
+import { HdObscureAddressPipe } from '@heavy-duty/wallet-adapter-cdk';
+import { WalletMultiButtonComponent } from './wallet-multi-button.component';
 
 @Component({
   standalone: true,
@@ -58,84 +49,15 @@ import { WalletsModalComponent } from './wallets-modal.component';
       </section>
 
       <section>
-        @if (connected()) {
-          <button
-            [disabled]="
-              disconnectWallet.disconnecting() || !disconnectWallet.wallet()
-            "
-            (click)="disconnectWallet.run()"
-            hdDisconnectWallet
-            #disconnectWallet="hdDisconnectWallet"
-            class="flex justify-center items-center gap-2 px-4 py-2 bg-red-400 rounded-md disabled:cursor-not-allowed"
-          >
-            @if (wallet(); as wallet) {
-              <hd-wallet-icon [hdWallet]="wallet"></hd-wallet-icon>
-            }
-
-            <span> {{ disconnectWallet.message() }} </span>
-          </button>
-        } @else if (wallet()) {
-          <button
-            [disabled]="
-              connectWallet.connecting() ||
-              !connectWallet.wallet() ||
-              connectWallet.connected()
-            "
-            (click)="connectWallet.run()"
-            hdConnectWallet
-            #connectWallet="hdConnectWallet"
-            class="flex gap-2 items-center px-4 py-2 bg-blue-500 rounded-md disabled:cursor-not-allowed"
-          >
-            @if (wallet(); as wallet) {
-              <hd-wallet-icon [hdWallet]="wallet"></hd-wallet-icon>
-            }
-
-            <span> {{ connectWallet.message() }} </span>
-          </button>
-        } @else {
-          <button
-            class="flex gap-2 items-center px-4 py-2 bg-blue-500 rounded-md disabled:cursor-not-allowed"
-            (click)="onSelectWallet()"
-          >
-            Select wallet
-          </button>
-        }
+        <hd-wallet-multi-button></hd-wallet-multi-button>
       </section>
     </main>
   `,
-  imports: [
-    NgClass,
-    HdDisconnectWalletDirective,
-    HdConnectWalletDirective,
-    HdWalletIconComponent,
-    HdObscureAddressPipe,
-    HdWalletIconComponent,
-  ],
+  imports: [NgClass, HdObscureAddressPipe, WalletMultiButtonComponent],
 })
 export class AppComponent {
-  private readonly _dialog = inject(Dialog);
-  private readonly _walletStore = inject(WalletStore);
   readonly wallet = injectWallet();
   readonly connected = injectConnected();
   readonly publicKey = injectPublicKey();
   readonly walletName = computed(() => this.wallet()?.adapter.name ?? 'None');
-
-  onSelectWallet() {
-    this._dialog
-      .open<WalletName | undefined, unknown, WalletsModalComponent>(
-        WalletsModalComponent
-      )
-      .closed.pipe(
-        concatMap((walletName) => {
-          if (!walletName) {
-            return EMPTY;
-          }
-
-          this._walletStore.selectWallet(walletName);
-
-          return this._walletStore.connect();
-        })
-      )
-      .subscribe();
-  }
 }
